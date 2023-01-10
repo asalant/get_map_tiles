f5eb01138c98c6a508b682f86b0e5c6e75614686
Alon Salant <alon@salant.org>
2023-01-09 16:05:42 -0800

    Generate urls for a map region at multiple zoom levels

--- lib/get_map_tiles/fetcher.rb
+++ lib/get_map_tiles/fetcher.rb
@@ -2,9 +2,13 @@
 
 module GetMapTiles
   class Fetcher
+    attr_accessor :url_template, :min_zoom, :max_zoom, :default_vars
+
     def initialize(url_template=nil, vars = {})
       @url_template = url_template
-      @vars = vars
+      @min_zoom = 4
+      @max_zoom = 16
+      @default_vars = vars
     end
 
     def get_tile_number(lat_deg, lng_deg, zoom)
@@ -17,8 +21,24 @@ module GetMapTiles
     end
 
     def format_tile_url(vars)
-      template_vars = {}.merge(@vars, vars)
+      template_vars = {}.merge(@default_vars, vars)
       sprintf(@url_template, template_vars)
+      
+    end
+
+    def get_urls_for_region(ne_corner, sw_corner)
+      urls = []
+      (@min_zoom .. @max_zoom).each do |zoom|
+        ne_tile = get_tile_number(ne_corner[:lat], ne_corner[:lon], zoom)
+        sw_tile = get_tile_number(sw_corner[:lat], sw_corner[:lon], zoom)
+
+        (ne_tile[:x] .. sw_tile[:x]).each do |x|
+          (ne_tile[:y] .. sw_tile[:y]).each do |y|
+            urls << format_tile_url(:x => x, :y => y, :z => zoom)
+          end
+        end
+      end
+      urls
     end
 
   end
--- test/fetcher_test.rb
+++ test/fetcher_test.rb
@@ -33,12 +33,15 @@ class FetcherTest < Minitest::Test
     assert_equal("http://server/14/1/2?abc", fetcher.format_tile_url(vars))
   end
 
-  def test_it_gets_urls_for_map_region
+  def test_it_gets_urls_for_region_at_14_zoom
     ne_corner = {:lat => 39.36786, :lon => -120.36380}
     sw_corner = {:lat => 39.35104, :lon => -120.31298}
     url_template = "http://server/%{z}/%{x}/%{y}?%{token}"
-    vars = {:x => 1, :y => 2, :z => 14, :token => 'abc'}
     fetcher = ::GetMapTiles::Fetcher.new(url_template, :token => 'abc')
+    fetcher.min_zoom = 14
+    fetcher.max_zoom = 15
+    urls = fetcher.get_urls_for_region(ne_corner, sw_corner)
+    assert_equal(21, urls.length)
   end
 end
 
